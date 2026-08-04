8c1fb361e1decbdab44626cd2875c4619492bb6a
GabrielYuta <outiyuta@gmail.com>
2026-08-04 09:30:46 -0300

    Add files via upload

---
+++ exercicio triangulo retamgulo.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlb.h>
+
+ int main (int argc, char *argv[] ){
+ 
+    int base, altura;
+    float area;
+    
+    base = 10;
+    altura = 20;
+    
+    area = (base*altura)/2;
+    
+    printf("A area do Triangulo Retangulo do Base = %d e altura e %f" , base, altura, area);
+
+ returm 0;
+ }
