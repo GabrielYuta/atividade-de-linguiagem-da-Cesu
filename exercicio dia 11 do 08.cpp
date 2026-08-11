f1a057f0aafd1c9f7315d7a93d7f1a3d6900219e
GabrielYuta <outiyuta@gmail.com>
2026-08-11 09:12:57 -0300

    Add files via upload

---
+++ exercicio dia 11 do 08.cpp
@@ -0,0 +1,22 @@
+#include <studio.h>
+#include <studlib.h>
+#define pi 3.141592
+
+
+int main(int argc, char* argv[]) {
+
+flor r, area;
+
+printf("insira o raio R  do circulo")
+scanf("%f",&r)
+
+r2 = pow(r,2);
+r2 = r*r;
+area = pi*r2;
+area = pi *(r*r);
+area = M-PI * pow(r,2);
+
+printf("A Area do Circulo de raio R %f = %f" , r, area)
+
+	return 0;
+}
